8f032dfcfec1b345fa813712cf312f44c7b629ea
Valeriya Litvinenko <ivlikovalera.ph@gmail.com>
2019-11-05 02:06:17 +0700

    Правит и создает компоненты, моки

--- src/components/list-of-cards/list-of-cards.jsx
+++ src/components/list-of-cards/list-of-cards.jsx
@@ -1,5 +1,49 @@
 import React from "react";
-import {Component} from "react";
+import {PureComponent} from "react";
 import {PropTypes as pt} from 'prop-types';
+import {CardOfPlace} from '../card-of-place/card-of-place.jsx';
 
-export default class ListOfCards extends Component {}
+export default class ListOfCards extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {isActiveCard: null};
+    this.cardPointHandler = this.cardPointHandle.bind(this);
+  }
+
+  cardPointHandle(id) {
+    this.setState({
+      isActiveCard: id
+    });
+  }
+
+  render() {
+    const {places} = this.props;
+    return <div className="cities__places-list places__list tabs__content">
+      {places.map((it) => <CardOfPlace
+        key={it.id}
+        id={it.id}
+        previewImage={it.previewImage}
+        title={it.title}
+        isPremium={it.isPremium}
+        rating={it.rating}
+        type={it.type}
+        price={it.price}
+        onCardPoint={this.cardPointHandler}
+      />)}
+    </div>;
+  }
+
+}
+
+ListOfCards.propTypes = {
+  onCardPoint: pt.func,
+  places: pt.array.isRequired,
+  offer: pt.shape({
+    previewImage: pt.string,
+    title: pt.string,
+    isPremium: pt.bool,
+    rating: pt.number,
+    type: pt.string,
+    price: pt.number,
+  })
+};
